28281eb85c84ec7f698258b4448beacad555c757
a7000q <a7000q@gmail.com>
2016-10-18 13:42:23 +0300

    w

--- models/TypeLimit.php
+++ models/TypeLimit.php
@@ -41,4 +41,63 @@ class TypeLimit extends \yii\db\ActiveRecord
             'name' => 'Name',
         ];
     }
+
+    public function getPeriod()
+    {
+        $res = "";
+        switch ($this->id) {
+            case 1:
+                $res = $this->dayPeriod();
+                break;
+            case 2:
+                $res = $this->weekPeriod();
+                break;
+            case 3:
+                $res = $this->monthPeriod();
+                break;
+            default:
+                $res = $this->dayPeriod();
+                break;
+        }
+
+        return $res;
+    }
+
+    public function dayPeriod()
+    {
+        $now_date = date("d.m.Y");
+
+        $d1 = strtotime($now_date." 00:00:00");
+        $d2 = strtotime($now_date." 23:59:59");
+
+        $result["start"] = $d1;
+        $result["end"] = $d2;
+
+        return $result;
+    }
+
+    public function weekPeriod()
+    {
+        $d1 = strtotime("last Monday");
+        $d2 = strtotime("next Thursday");
+
+        $d1 = date("d.m.Y", $d1)." 00:00:00";
+        $d2 = date("d.m.Y", $d2)." 23:59:59";
+
+        $result["start"] = strtotime($d1);
+        $result["end"] = strtotime($d2);
+
+        return $result;
+    }
+
+    public function monthPeriod()
+    {
+        $d1 = date("1.m.Y 00:00:00");
+        $d2 = date("t.m.Y 23:59:59");
+
+        $result["start"] = strtotime($d1);
+        $result["end"] = strtotime($d2);
+
+        return $result;
+    }
 }
